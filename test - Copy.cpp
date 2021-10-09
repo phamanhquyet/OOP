ccea17d7a7a2818cb0febb3685356e123b73d0bc
unknown <q24052002@gmail.com>
2021-10-10 03:37:35 +0700

    phamanhquyet

---
+++ test - Copy.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1;
+    float b1,b2;
+    b1=(float)(a-32)*5/9;
+    b2=(float)(a-32)*((float)5/9);
+    cout<<"b1 = "<<b1<<endl;
+    cout<<"b2 = " <<b2<<endl;
+}
+
